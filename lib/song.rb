2c8f0fff77df52c6a6be704d1ee860b7c3ef63a1
Evan Jeffries <ejeffries14@gmail.com>
2019-12-12 06:26:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,7 @@
+require "pry"
+
 class Song
-  attr_accessor :name, :artist_name
+  attr_accessor :artist_name, :name
   @@all = []
 
   def self.all
@@ -10,4 +12,60 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(song_name)
+    s = self.new
+    s.name = song_name
+    s
+  end
+
+  def self.create_by_name(song_name)
+    s = self.new
+    s.name = song_name
+    s
+    @@all << s
+    s
+  end
+
+  def self.find_by_name(title)
+    Song.all.find do |i|
+      if title == i.name
+        title
+      end
+    end
+  end
+
+  def self.find_or_create_by_name(title)
+    if title == Song.find_by_name(title)
+      Song.find_by_name(title)
+    elsif
+      Song.create_by_name(title)
+    end
+    Song.find_by_name(title) || Song.create_by_name(title)
+  end
+
+  def self.alphabetical
+    Song.all.sort_by do |a|
+      a.name
+    end
+  end
+
+  def self.new_from_filename(filename)
+    i = filename.split(" . ")
+    i.collect do |arr|
+      data = arr.split(" - ")
+      name = data[1]
+      artist_name = data[0]
+
+      song = Song.new
+      song.name = name
+      song.artist_name = artist_name
+      song
+    end
+  end
 end
